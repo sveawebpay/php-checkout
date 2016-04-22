637338cd280860ae318fba495525b4e9282ed93e
Savo <savo.garovic@svea.com>
2016-04-22 15:05:25 +0200

    Create TestCase Class - prepare test data in one place

---
+++ tests/Unit/TestCase.php
@@ -0,0 +1,125 @@
+<?php
+
+
+namespace Svea\Checkout\Tests\Unit;
+
+
+use Svea\Checkout\Transport\ApiClient;
+use Svea\Checkout\Transport\Connector;
+use Svea\Checkout\Transport\Http\HttpRequestInterface;
+use Svea\Checkout\Transport\Request;
+use Svea\Checkout\Transport\ResponseHandler;
+
+class TestCase extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ResponseHandler $response
+     */
+    protected $response;
+
+    /**
+     * @var Request $request
+     */
+    protected $request;
+
+    /**
+     * @var Connector $connector
+     */
+    protected $connector;
+
+    /**
+     * @var ApiClient $apiClient
+     */
+    protected $apiClient;
+
+    /**
+     * @var HttpRequestInterface $httpClientMock
+     */
+    protected $httpClientMock;
+
+    // Response
+    protected $responseContent;
+    protected $jsonResponseContent;
+
+    // Client Credentials
+    protected $merchantId = '123456';
+    protected $sharedSecret = '80e3a905e597ca428f4e25200433263c';
+    protected $apiUrl = Connector::TEST_BASE_URL;
+
+    protected $orderData = array(
+        "purchase_country" => "gb",
+        "purchase_currency" => "gbp",
+        "locale" => "en-gb",
+        "order_amount" => 10000,
+        "order_tax_amount" => 2000,
+        "order_lines" => array(
+            array(
+                "type" => "physical",
+                "reference" => "123050",
+                "name" => "Tomatoes",
+                "quantity" => 10,
+                "quantity_unit" => "kg",
+                "unit_price" => 600,
+                "tax_rate" => 2500,
+                "total_amount" => 6000,
+                "total_tax_amount" => 1200
+            ),
+            array(
+                "type" => "physical",
+                "reference" => "543670",
+                "name" => "Bananas",
+                "quantity" => 1,
+                "quantity_unit" => "bag",
+                "unit_price" => 5000,
+                "tax_rate" => 2500,
+                "total_amount" => 4000,
+                "total_discount_amount" => 1000,
+                "total_tax_amount" => 800
+            )
+        ),
+        "merchant_urls" => array(
+            "terms" => "http://www.merchant.com/toc",
+            "checkout" => "http://www.merchant.com/checkout?klarna_order_id={checkout.order.id}",
+            "confirmation" => "http://www.merchant.com/thank-you?klarna_order_id={checkout.order.id}",
+            "push" => "http://www.merchant.com/create_order?klarna_order_id={checkout.order.id}"
+        )
+    );
+
+    protected function setUp()
+    {
+        $this->responseContent = array('data' => 'This is content');
+        $this->jsonResponseContent = json_encode($this->responseContent);
+
+        $this->setRequest();
+        $this->setCurlRequest();
+        $this->setApiClient();
+        $this->setConnector();
+    }
+
+    private function setRequest()
+    {
+        $this->request = new Request();
+        $this->request->setApiUrl($this->apiUrl);
+        $this->request->setBody(json_encode($this->orderData));
+        $this->request->setPostMethod();
+        $this->request->setAuthorizationToken('123456789');
+    }
+
+    private function setCurlRequest()
+    {
+        $this->httpClientMock = $this->getMockBuilder('Svea\Checkout\Transport\Http\HttpRequestInterface')->getMock();
+    }
+
+    private function setApiClient()
+    {
+        $httpClientMock = $this->getMockBuilder('Svea\Checkout\Transport\Http\HttpRequestInterface')->getMock();
+        $this->apiClient = $this->getMockBuilder('\Svea\Checkout\Transport\ApiClient')
+            ->setConstructorArgs(array($httpClientMock))
+            ->getMock();
+    }
+
+    private function setConnector()
+    {
+        $this->connector = Connector::create($this->merchantId, $this->sharedSecret, $this->apiUrl);
+    }
+}
--- tests/Unit/Transport/ApiClientTest.php
+++ tests/Unit/Transport/ApiClientTest.php
@@ -1,83 +1,19 @@
 <?php
 
 
-namespace Unit\Transport;
+namespace Svea\Checkout\Tests\Unit\Transport;
 
 
+use Svea\Checkout\Tests\Unit\TestCase;
 use Svea\Checkout\Transport\ApiClient;
-use Svea\Checkout\Transport\Connector;
-use Svea\Checkout\Transport\Request;
 use Svea\Checkout\Transport\ResponseHandler;
 
-class ClientTest extends \PHPUnit_Framework_TestCase
+class ApiClientTest extends TestCase
 {
-    private $merchantId = '123456';
-    private $sharedSecret = '80e3a905e597ca428f4e25200433263c';
-    private $apiUrl = Connector::TEST_BASE_URL;
-    private $orderData = array(
-        "purchase_country" => "gb",
-        "purchase_currency" => "gbp",
-        "locale" => "en-gb",
-        "order_amount" => 10000,
-        "order_tax_amount" => 2000,
-        "order_lines" => array(
-            array(
-                "type" => "physical",
-                "reference" => "123050",
-                "name" => "Tomatoes",
-                "quantity" => 10,
-                "quantity_unit" => "kg",
-                "unit_price" => 600,
-                "tax_rate" => 2500,
-                "total_amount" => 6000,
-                "total_tax_amount" => 1200
-            ),
-            array(
-                "type" => "physical",
-                "reference" => "543670",
-                "name" => "Bananas",
-                "quantity" => 1,
-                "quantity_unit" => "bag",
-                "unit_price" => 5000,
-                "tax_rate" => 2500,
-                "total_amount" => 4000,
-                "total_discount_amount" => 1000,
-                "total_tax_amount" => 800
-            )
-        ),
-        "merchant_urls" => array(
-            "terms" => "http://www.merchant.com/toc",
-            "checkout" => "http://www.merchant.com/checkout?klarna_order_id={checkout.order.id}",
-            "confirmation" => "http://www.merchant.com/thank-you?klarna_order_id={checkout.order.id}",
-            "push" => "http://www.merchant.com/create_order?klarna_order_id={checkout.order.id}"
-        )
-    );
-
-    protected $request;
-    protected $responseContent;
-    protected $jsonResponseContent;
-    protected $httpClientMock;
-
-    protected function setUp()
-    {
-        $this->request = new Request();
-        $this->request->setApiUrl($this->apiUrl);
-        $this->request->setBody(json_encode($this->orderData));
-        $this->request->setPostMethod();
-        $this->request->setAuthorizationToken('123456789');
-
-        $this->responseContent = array('data' => 'This is content');
-        $this->jsonResponseContent = json_encode($this->responseContent);
-
-        $this->httpClientMock = $this->getMockBuilder('Svea\Checkout\Transport\Http\HttpRequestInterface')->getMock();
-    }
-
     protected function setHttpClient()
     {
-        $client = new ApiClient($this->httpClientMock);
-        $client->setHttpClient($this->httpClientMock);
-
-        return $client;
+        $this->apiClient = new ApiClient($this->httpClientMock);
+        $this->apiClient->setHttpClient($this->httpClientMock);
     }
 
     public function testSendRequestWithOkStatusResponse()
@@ -93,12 +29,12 @@ class ClientTest extends \PHPUnit_Framework_TestCase
             ->with(CURLINFO_HTTP_CODE)
             ->will($this->returnValue(200));
 
-        $client = $this->setHttpClient();
+        $this->setHttpClient();
 
         /**
          * @var ResponseHandler $response
          */
-        $response = $client->sendRequest($this->request);
+        $response = $this->apiClient->sendRequest($this->request);
 
         $this->assertInstanceOf('Svea\Checkout\Transport\ResponseHandler', $response);
         $this->assertEquals($this->responseContent, $response->getContent());
@@ -117,17 +53,21 @@ class ClientTest extends \PHPUnit_Framework_TestCase
             ->with(CURLINFO_HTTP_CODE)
             ->will($this->returnValue(201));
 
-        $client = $this->setHttpClient();
+        $this->setHttpClient();
 
         /**
          * @var ResponseHandler $response
          */
-        $response = $client->sendRequest($this->request);
+        $response = $this->apiClient->sendRequest($this->request);
 
         $this->assertInstanceOf('Svea\Checkout\Transport\ResponseHandler', $response);
         $this->assertEquals($this->responseContent, $response->getContent());
     }
 
+    /**
+     * @expectedException \Svea\Checkout\Transport\Exception\SveaApiException
+     * @expectedExceptionMessage The input data was bad
+     */
     public function testSendRequestWithBadRequestStatusResponse()
     {
         $this->httpClientMock->expects($this->once())
@@ -141,14 +81,14 @@ class ClientTest extends \PHPUnit_Framework_TestCase
             ->with(CURLINFO_HTTP_CODE)
             ->will($this->returnValue(400));
 
-        $client = $this->setHttpClient();
-
-
-        $this->setExpectedException('\Svea\Checkout\Transport\Exception\SveaApiException', 'The input data was bad');
-
-        $client->sendRequest($this->request);
+        $this->setHttpClient();
+        $this->apiClient->sendRequest($this->request);
     }
 
+    /**
+     * @expectedException \Svea\Checkout\Transport\Exception\SveaApiException
+     * @expectedExceptionMessage No order with requested ID was found.
+     */
     public function testSendRequestWithNotFoundStatusResponse()
     {
         $this->httpClientMock->expects($this->once())
@@ -162,14 +102,14 @@ class ClientTest extends \PHPUnit_Framework_TestCase
             ->with(CURLINFO_HTTP_CODE)
             ->will($this->returnValue(404));
 
-        $client = $this->setHttpClient();
-
-
-        $this->setExpectedException('\Svea\Checkout\Transport\Exception\SveaApiException', 'No order with requested ID was found.');
-
-        $client->sendRequest($this->request);
+        $this->setHttpClient();
+        $this->apiClient->sendRequest($this->request);
     }
 
+    /**
+     * @expectedException \Svea\Checkout\Transport\Exception\SveaApiException
+     * @expectedExceptionMessage Unauthorized: Missing or incorrect Authorization token in header.
+     */
     public function testSendRequestWithUnauthorizedStatusResponse()
     {
         $this->httpClientMock->expects($this->once())
@@ -183,16 +123,14 @@ class ClientTest extends \PHPUnit_Framework_TestCase
             ->with(CURLINFO_HTTP_CODE)
             ->will($this->returnValue(401));
 
-        $client = $this->setHttpClient();
-
-
-        $this->setExpectedException('\Svea\Checkout\Transport\Exception\SveaApiException',
-            'Unauthorized: Missing or incorrect Authorization token in header. ' .
-            'Please verify that you used the correct Merchant ID and Shared secret when you constructed your client.');
-
-        $client->sendRequest($this->request);
+        $this->setHttpClient();
+        $this->apiClient->sendRequest($this->request);
     }
 
+    /**
+     * @expectedException \Svea\Checkout\Transport\Exception\SveaApiException
+     * @expectedExceptionMessage Undefined error.
+     */
     public function testSendRequestWithUndefinedStatusResponse()
     {
         $this->httpClientMock->expects($this->once())
@@ -206,24 +144,21 @@ class ClientTest extends \PHPUnit_Framework_TestCase
             ->with(CURLINFO_HTTP_CODE)
             ->will($this->returnValue(''));
 
-        $client = $this->setHttpClient();
-
-
-        $this->setExpectedException('\Svea\Checkout\Transport\Exception\SveaApiException', 'Undefined error.');
-
-        $client->sendRequest($this->request);
+        $this->setHttpClient();
+        $this->apiClient->sendRequest($this->request);
     }
 
+    /**
+     * @expectedException \Exception
+     * @expectedExceptionMessage Could not resolve host: rarafsafsafasfas.com
+     */
     public function testSendRequestWithHttpClientError()
     {
         $this->httpClientMock->expects($this->once())
             ->method('getError')
             ->will($this->returnValue('Could not resolve host: rarafsafsafasfas.com'));
 
-        $client = $this->setHttpClient();
-
-        $this->setExpectedException('\Exception', 'Could not resolve host: rarafsafsafasfas.com');
-
-        $client->sendRequest($this->request);
+        $this->setHttpClient();
+        $this->apiClient->sendRequest($this->request);
     }
 }
