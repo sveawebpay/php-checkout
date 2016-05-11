e36b737833eccc35fb3b29e539b5f770833da52c
Janko Stevanović <rs-janstev-01@corp.svea.com>
2016-05-11 15:07:46 +0200

    add php docs for get-order

--- examples/get-order.php
+++ examples/get-order.php
@@ -1,34 +1,83 @@
 <?php
 
+// include the Svea Checkout autoload file if you are not using Composer
 require_once '../vendor/autoload.php';
 
-// Order ID sample
+/*
+ * Example of getting the order information
+ *
+ * */
+
+// Order ID from created order
 $orderId = 9;
 
-echo '<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>';
 
-try {
-    $merchantId = '1';
-    $sharedSecret = 'sharedSecret';
-    $baseUrl = \Svea\Checkout\Transport\Connector::TEST_BASE_URL;
+/*
+ * Create connector for given
+ *  - Merchant Id - unique merchant ID
+ *  - Shared Secret - Shared Secret string between Svea and merchant
+ *  - Base Url for SVEA Api. Can be TEST_BASE_URL and PROD_BASE_URL
+ * */
+$merchantId = '1';
+$sharedSecret = 'sharedSecret';
+$baseUrl = \Svea\Checkout\Transport\Connector::TEST_BASE_URL;
 
-    $conn = \Svea\Checkout\Transport\Connector::init($merchantId, $sharedSecret, $baseUrl);
+/*
+ * Create Connector object
+ *
+ * Exception \Svea\Checkout\Exception\SveaConnectorException will be returned if
+ * some of fields $merchantId, $sharedSecret and $baseUrl is missing
+ * */
+$conn = \Svea\Checkout\Transport\Connector::init($merchantId, $sharedSecret, $baseUrl);
 
-    $checkoutClient = new \Svea\Checkout\CheckoutClient($conn);
-    $response = $checkoutClient->get($orderId);
+// Create Checkout client with created Connector object
+$checkoutClient = new \Svea\Checkout\CheckoutClient($conn);
 
-    echo "<pre>" . print_r($response, true) . "</pre>";
-} catch (\Svea\Checkout\Exception\SveaApiException $ex) {
-    var_dump("--------- Api errors ---------");
-    var_dump($ex->getMessage());
-} catch (\Svea\Checkout\Exception\SveaConnectorException $ex) {
-    var_dump("--------- Conn errors ---------");
-    var_dump($ex->getMessage());
-} catch (\Svea\Checkout\Exception\SveaInputValidationException $ex) {
-    var_dump("--------- Input data errors ---------");
-    var_dump($ex->getMessage());
-} catch (Exception $ex) {
-    var_dump("--------- General errors ---------");
-    var_dump($ex->getMessage());
-}
+/*
+ *  Initialize getting the order information
+ *  Possible Exceptions are:
+ *  - \Svea\Checkout\Exception\SveaInputValidationException - if $orderId is missing
+ *  - \Svea\Checkout\Exception\SveaApiException - is there is some problem with api connection or
+ *      some error occurred with data validation on API side
+ *  - \Exception - for any other error
+ *
+ * */
+$response = $checkoutClient->get($orderId);
 
+
+/*
+ * Format of returned response array
+ *
+ *  - MerchantSettings
+ *      - TermsUri
+ *      - CheckoutUri
+ *      - ConfirmationUri
+ *      - PushUri
+ *  - Cart
+ *      - Items [..] / list of items
+ *          - ArticleNumber
+ *          - Name
+ *          - Quantity
+ *          - UnitPrice
+ *          - DiscountPercent
+ *          - VatPercent
+ *          - Unit
+ *  - Customer
+ *  - ShippingAddress
+ *  - BillingAddress
+ *  - Gui
+ *      - Layout
+ *      - Snippet
+ *  - Locale
+ *  - Currency
+ *  - CountryCode
+ *  - PresetValues
+ *  - OrderId
+ *  - Status
+ * */
+
+$orderId = $response['OrderId'];
+
+$guiSnippet = $response['Gui']['Snippet'];
+
+$orderStatus = $response['Status'];
