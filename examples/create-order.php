c17341966df5a0c7c100632c6ad01e021e8d080d
Janko Stevanović <rs-janstev-01@corp.svea.com>
2016-05-11 11:34:13 +0200

    php docs for create-order example

--- examples/create-order.php
+++ examples/create-order.php
@@ -1,8 +1,20 @@
 <?php
 
+// include the Svea Checkout autoload file if you are not using Composer
 require_once '../vendor/autoload.php';
 
 
+/*
+ * Example of creating order and getting response data
+ *
+ * Possible Throw Exceptions are:
+ *  - \Svea\Checkout\Exception\SveaApiException when ...
+ *  - \Svea\Checkout\Exception\SveaInputValidationException
+ *  - \Exception
+ *
+ * */
+
+// - Add required information for creating order
 $data = array(
     "purchase_country" => "SE",
     "purchase_currency" => "SEK",
@@ -45,17 +57,63 @@ $data = array(
 );
 
 
+/*
+ * Create connector for given
+ *  - Merchant Id
+ *  - Shared Secret
+ *  - Base Url for SVEA Api
+ * */
 $merchantId = '1';
 $sharedSecret = 'sharedSecret';
 $baseUrl = \Svea\Checkout\Transport\Connector::TEST_BASE_URL;
 
+/*
+ * Create Connector object
+ *
+ * Possible Throw Exception is \Svea\Checkout\Exception\SveaConnectorException which will return exception if
+ * some of fields $merchantId, $sharedSecret and $baseUrl is missing
+ * */
 $conn = \Svea\Checkout\Transport\Connector::init($merchantId, $sharedSecret, $baseUrl);
 
+// Create Checkout client with created Connector object
 $checkoutClient = new \Svea\Checkout\CheckoutClient($conn);
+
+// Initialize creating order and receive response data
 $response = $checkoutClient->create($data);
 
-/* 
+/*
+ * Format of returned response array
+ *
+ *  - MerchantSettings
+ *      - TermsUri
+ *      - CheckoutUri
+ *      - ConfirmationUri
+ *      - PushUri
+ *  - Cart
+ *      - Items [..] / list of items
+ *          - ArticleNumber
+ *          - Name
+ *          - Quantity
+ *          - UnitPrice
+ *          - DiscountPercent
+ *          - VatPercent
+ *          - Unit
+ *  - Customer
+ *  - ShippingAddress
+ *  - BillingAddress
+ *  - Gui
+ *      - Layout
+ *      - Snippet
+ *  - Locale
+ *  - Currency
+ *  - CountryCode
+ *  - PresetValues
+ *  - OrderId
+ *  - Status
  * */
 
-echo "<pre>" . print_r($response, true) . "</pre>";
+$orderId = $response['OrderId'];
 
+$guiSnippet = $response['Gui']['Snippet'];
+
+$orderStatus = $response['Status'];
