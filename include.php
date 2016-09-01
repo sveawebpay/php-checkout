832c02f60583140e95d906ab83eb28e5f2de7e2a
Savo <savo.garovic@svea.com>
2016-09-01 16:39:26 +0200

    includes.php - fixed paths problem

--- include.php
+++ include.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- *
- * This file is used if you want to autoload project classes without using Composer
- * */
-
-
 /**
  * Return desired class
  * @param string $className
@@ -13,8 +7,7 @@
 function __autoload_checkout_connection_classes($className)
 {
     $filename = str_replace('Svea\\Checkout\\', '', $className);
-
-    $fullPath = __DIR__ . '\\src\\' . $filename . ".php";
+    $fullPath = str_replace('\\', '/', __DIR__ . '\\src\\' . $filename . ".php");
 
     if (file_exists($fullPath)) {
         include_once $fullPath;
