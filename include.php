5541f12aef658416bac8e840d5acb6bb16b738b2
Savo <savo.garovic@svea.com>
2016-10-27 15:15:28 +0200

    Fix includes.php file

--- include.php
+++ include.php
@@ -11,10 +11,10 @@
  */
 function __autoload_svea_checkout_connection_classes($className)
 {
-	/* Skip other namespaces */
-	if (!preg_match('#^(Svea\\\\Checkout)#', $className)) {
-		return;
-	}
+    /* Skip other namespaces */
+    if (!preg_match('#^(Svea\\\\Checkout)#', $className)) {
+        return;
+    }
 
     $filename = str_replace('Svea\\Checkout\\', '', $className);
     $fullPath = str_replace('\\', '/', __DIR__ . '\\src\\' . $filename . ".php");
