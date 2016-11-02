2b80d893683ec33f88cf56b350dfd32ca028ca62
Savo <savo.garovic@svea.com>
2016-11-02 15:57:50 +0100

    Add Update order row (in progress...)

---
+++ src/Implementation/Admin/UpdateOrderRow.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Svea\Checkout\Implementation\Admin;
+
+class UpdateOrderRow extends AdminImplementationManager
+{
+
+    /**
+     * Input data validation
+     * @param mixed $data Input data to Svea Checkout Library
+     */
+    public function validateData($data)
+    {
+        // TODO: Implement validateData() method.
+    }
+
+    /**
+     * Prepare body data for Api call
+     * @param mixed $data
+     */
+    public function prepareData($data)
+    {
+        // TODO: Implement prepareData() method.
+    }
+
+    /**
+     * Invoke Api call
+     */
+    public function invoke()
+    {
+        // TODO: Implement invoke() method.
+    }
+}
