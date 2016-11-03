44602d1073c63e19c2bfdda2bf2b74e10dbdc74e
Savo <savo.garovic@svea.com>
2016-11-03 14:46:36 +0100

    Add Unit tests for Validation admin functions

--- tests/Unit/Validation/Admin/ValidateCancelOrderAmountDataTest.php
+++ tests/Unit/Validation/Admin/ValidateCancelOrderAmountDataTest.php
@@ -7,26 +7,134 @@ use Svea\Checkout\Validation\Admin\ValidateCancelOrderAmountData;
 
 class ValidateCancelOrderAmountDataTest extends TestCase
 {
-    public function testValidate()
+    /**
+     * @var ValidateCancelOrderAmountData $validateUpdateOrderData
+     */
+    private $validateCancelOrderAmount;
+
+    /**
+     * @var mixed $inputData
+     */
+    private $inputData;
+
+    public function setUp()
     {
-        $inputData = array(
-            'orderid' => 201,
-            'amount' => 1,
+        parent::setUp();
+        $this->validateCancelOrderAmount = new ValidateCancelOrderAmountData();
+
+        $this->inputData = array(
+            'orderid' => 204,
+            'amount' => 500
         );
+    }
 
-        /**
-         * @var ValidateCancelOrderAmountData|\PHPUnit_Framework_MockObject_MockObject $mock
-         */
-        $mock = $this->getMockBuilder('\Svea\Checkout\Validation\Admin\ValidateCancelOrderAmountData')
-            ->setMethods(array('mustBeSet', 'mustBeInteger'))
-            ->getMock();
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithoutOrderId()
+    {
+        unset($this->inputData['orderid']);
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
 
-        $mock->expects($this->exactly(2))
-            ->method('mustBeSet');
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithOrderIdAsString()
+    {
+        $this->inputData['orderid'] = '204';
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
 
-        $mock->expects($this->exactly(2))
-            ->method('mustBeInteger');
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithEmptyOrderId()
+    {
+        $this->inputData['orderid'] = '';
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
 
-        $mock->validate($inputData);
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithNullOrderId()
+    {
+        $this->inputData['orderid'] = null;
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithOrderIdAsDecimal()
+    {
+        $this->inputData['orderid'] = 204.5;
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
+
+    public function testValidateWithOrderIdAsInteger()
+    {
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithoutAmount()
+    {
+        unset($this->inputData['amount']);
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithAmountAsString()
+    {
+        $this->inputData['amount'] = '204';
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithEmptyAmount()
+    {
+        $this->inputData['amount'] = '';
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithNullAmount()
+    {
+        $this->inputData['amount'] = null;
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithAmountAsDecimal()
+    {
+        $this->inputData['amount'] = 204.5;
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
+    }
+
+    public function testValidateWithAmountAsInteger()
+    {
+        $this->invokeMethod($this->validateCancelOrderAmount, 'validate', array($this->inputData));
     }
 }
--- tests/Unit/Validation/Admin/ValidateCancelOrderRowDataTest.php
+++ tests/Unit/Validation/Admin/ValidateCancelOrderRowDataTest.php
@@ -7,26 +7,134 @@ use Svea\Checkout\Validation\Admin\ValidateCancelOrderRowData;
 
 class ValidateCancelOrderRowDataTest extends TestCase
 {
-    public function testValidate()
+    /**
+     * @var ValidateCancelOrderRowData $validateUpdateOrderData
+     */
+    private $validateCancelOrderRow;
+
+    /**
+     * @var mixed $inputData
+     */
+    private $inputData;
+
+    public function setUp()
     {
-        $inputData = array(
+        parent::setUp();
+        $this->validateCancelOrderRow = new ValidateCancelOrderRowData();
+
+        $this->inputData = array(
             'orderid' => 201,
             'orderrowid' => 1,
         );
+    }
 
-        /**
-         * @var ValidateCancelOrderRowData|\PHPUnit_Framework_MockObject_MockObject $mock
-         */
-        $mock = $this->getMockBuilder('\Svea\Checkout\Validation\Admin\ValidateCancelOrderRowData')
-            ->setMethods(array('mustBeSet', 'mustBeInteger'))
-            ->getMock();
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithoutOrderId()
+    {
+        unset($this->inputData['orderid']);
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
 
-        $mock->expects($this->exactly(2))
-            ->method('mustBeSet');
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithOrderIdAsString()
+    {
+        $this->inputData['orderid'] = '204';
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
 
-        $mock->expects($this->exactly(2))
-            ->method('mustBeInteger');
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithEmptyOrderId()
+    {
+        $this->inputData['orderid'] = '';
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
 
-        $mock->validate($inputData);
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithNullOrderId()
+    {
+        $this->inputData['orderid'] = null;
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithOrderIdAsDecimal()
+    {
+        $this->inputData['orderid'] = 204.5;
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
+
+    public function testValidateWithOrderIdAsInteger()
+    {
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithoutOrderRowId()
+    {
+        unset($this->inputData['orderrowid']);
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithOrderRowIdAsString()
+    {
+        $this->inputData['orderrowid'] = '204';
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithEmptyOrderRowId()
+    {
+        $this->inputData['orderrowid'] = '';
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithNullOrderRowId()
+    {
+        $this->inputData['orderrowid'] = null;
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
+
+    /**
+     * @expectedException \Svea\Checkout\Exception\SveaInputValidationException
+     * @expectedExceptionCode Svea\Checkout\Exception\ExceptionCodeList::INPUT_VALIDATION_ERROR
+     */
+    public function testValidateWithOrderRowIdAsDecimal()
+    {
+        $this->inputData['orderrowid'] = 204.5;
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
+    }
+
+    public function testValidateWithOrderRowIdAsInteger()
+    {
+        $this->invokeMethod($this->validateCancelOrderRow, 'validate', array($this->inputData));
     }
 }
