d15b7d3d6cfffebd7568dcf8aeaae414d7b9b51a
fre-sund <fredrik.sundell@sveaekonomi.se>
2019-03-25 14:58:42 +0100

    Check if merchantdata is included before using it

--- src/Implementation/UpdateOrder.php
+++ src/Implementation/UpdateOrder.php
@@ -35,7 +35,9 @@ class UpdateOrder extends ImplementationManager
     {
         $cart = array();
         $cart['cart'] = $data['cart'];
-        $cart['merchantdata'] = $data['merchantdata'];
+        if(isset($data['merchantdata'])) {
+            $cart['merchantdata'] = $data['merchantdata'];
+        }
         $orderId = $data['orderid'];
         $this->requestModel = new Request();
         $this->requestModel->setPutMethod();
